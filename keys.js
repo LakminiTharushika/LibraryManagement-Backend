24deeaf28eeda686fa44f1a92c82e18258202b89
Lakmini Tharushika <96492@fhss.sjp.ac.lk>
2023-02-28 23:45:54 +0530

    initial commit

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URI:
+        "mongodb+srv://dbUser:dbUserlib@librarymanagement.umqpwkb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET: "MOHENMONDALJDFKFLL",
+};
