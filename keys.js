c85fb1e647e0a3d9b1d15b56974975c51615d3ed
Lakmini Tharushika <96492@fhss.sjp.ac.lk>
2023-03-08 22:20:43 +0530

    configuration changes

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     MONGO_URI:
-        "mongodb+srv://dbUser:dbUserlib@librarymanagement.umqpwkb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        "mongodb+srv://dbUser:dbUserlib@librarymanagement.umqpwkb.mongodb.net/LibraryManagement?retryWrites=true&w=majority",
     JWT_SECRET: "MOHENMONDALJDFKFLL",
 };
